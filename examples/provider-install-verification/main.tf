427a303fd13a20942816a7f3a3de02ff3d773cae
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-07-14 16:29:45 +0200

    Example password

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
-provider "mongodb" {}
+provider "mongodb" {
+  host = "http://localhost:19090"
+  username = "education"
+  password = "test123"
+}
 
 data "mongodb_coffees" "example" {}
