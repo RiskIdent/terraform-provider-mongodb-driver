bc3e3250c0678a0af73c6bab315348e7842b2fe8
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-07-25 11:20:48 +0200

    Refactored mongodb methods

--- internal/mongodb/mongodb.go
+++ internal/mongodb/mongodb.go
@@ -158,32 +158,41 @@ type User struct {
 }
 
 func (c *Client) ListDBUsers(ctx context.Context, dbName string, filter any) ([]User, error) {
-	query := bson.D{
-		{Key: "usersInfo", Value: 1}, // 1 = all in current db
+	if err := c.connect(ctx); err != nil {
+		return nil, err
 	}
-	if filter != nil {
-		query = append(query, bson.E{Key: "filter", Value: filter})
+	query := usersInfoCommand{
+		UsersInfo: 1, // 1 = all in current db
+		Filter:    filter,
 	}
-	return c.listUsers(ctx, dbName, query)
+	return c.runUsersInfo(ctx, dbName, query)
 }
 
 func (c *Client) ListAllUsers(ctx context.Context, filter any) ([]User, error) {
-	query := bson.D{
-		{Key: "usersInfo", Value: bson.D{
+	if err := c.connect(ctx); err != nil {
+		return nil, err
+	}
+	query := usersInfoCommand{
+		UsersInfo: bson.D{
 			{Key: "forAllDBs", Value: true},
-		}},
+		},
+		Filter: filter,
 	}
-	if filter != nil {
-		query = append(query, bson.E{Key: "filter", Value: filter})
-	}
-	return c.listUsers(ctx, "admin", query)
+	return c.runUsersInfo(ctx, "admin", query)
 }
 
 func (c *Client) GetDBUser(ctx context.Context, dbName, userName string) (User, error) {
-	query := bson.D{
-		{Key: "usersInfo", Value: userName},
+	if err := c.connect(ctx); err != nil {
+		return User{}, err
 	}
-	users, err := c.listUsers(ctx, dbName, query)
+	return c.runUsersInfoSingle(ctx, dbName, userName)
+}
+
+func (c *Client) runUsersInfoSingle(ctx context.Context, dbName, userName string) (User, error) {
+	query := usersInfoCommand{
+		UsersInfo: userName,
+	}
+	users, err := c.runUsersInfo(ctx, dbName, query)
 	if err != nil {
 		return User{}, err
 	}
@@ -193,10 +202,12 @@ func (c *Client) GetDBUser(ctx context.Context, dbName, userName string) (User, 
 	return users[0], nil
 }
 
-func (c *Client) listUsers(ctx context.Context, dbName string, query bson.D) ([]User, error) {
-	if err := c.connect(ctx); err != nil {
-		return nil, err
-	}
+type usersInfoCommand struct {
+	UsersInfo any `bson:"usersInfo"`
+	Filter    any `bson:"filter,omitempty"`
+}
+
+func (c *Client) runUsersInfo(ctx context.Context, dbName string, query usersInfoCommand) ([]User, error) {
 	db := c.client.Database(dbName)
 
 	result := db.RunCommand(ctx, query)
@@ -225,20 +236,20 @@ type NewUser struct {
 }
 
 func (c *Client) CreateDBUser(ctx context.Context, dbName string, newUser NewUser) (User, error) {
-	if err := c.createDBUser(ctx, dbName, newUser); err != nil {
+	if err := c.connect(ctx); err != nil {
 		return User{}, err
 	}
-	user, err := c.GetDBUser(ctx, dbName, newUser.User)
+	if err := c.runCreateUser(ctx, dbName, newUser); err != nil {
+		return User{}, err
+	}
+	user, err := c.runUsersInfoSingle(ctx, dbName, newUser.User)
 	if err != nil {
 		return User{}, fmt.Errorf("get created user: %w", err)
 	}
 	return user, nil
 }
 
-func (c *Client) createDBUser(ctx context.Context, dbName string, newUser NewUser) error {
-	if err := c.connect(ctx); err != nil {
-		return err
-	}
+func (c *Client) runCreateUser(ctx context.Context, dbName string, newUser NewUser) error {
 	db := c.client.Database(dbName)
 
 	result := db.RunCommand(ctx, newUser)
@@ -264,20 +275,20 @@ type UpdateUser struct {
 }
 
 func (c *Client) UpdateDBUser(ctx context.Context, dbName string, update UpdateUser) (User, error) {
-	if err := c.updateDBUser(ctx, dbName, update); err != nil {
+	if err := c.connect(ctx); err != nil {
 		return User{}, err
 	}
-	user, err := c.GetDBUser(ctx, dbName, update.User)
+	if err := c.runUpdateUser(ctx, dbName, update); err != nil {
+		return User{}, err
+	}
+	user, err := c.runUsersInfoSingle(ctx, dbName, update.User)
 	if err != nil {
 		return User{}, fmt.Errorf("get updated user: %w", err)
 	}
 	return user, nil
 }
 
-func (c *Client) updateDBUser(ctx context.Context, dbName string, update UpdateUser) error {
-	if err := c.connect(ctx); err != nil {
-		return err
-	}
+func (c *Client) runUpdateUser(ctx context.Context, dbName string, update UpdateUser) error {
 	db := c.client.Database(dbName)
 
 	result := db.RunCommand(ctx, update)
@@ -298,6 +309,10 @@ func (c *Client) DeleteDBUser(ctx context.Context, dbName, userName string) erro
 	if err := c.connect(ctx); err != nil {
 		return err
 	}
+	return c.runDropUser(ctx, dbName, userName)
+}
+
+func (c *Client) runDropUser(ctx context.Context, dbName, userName string) error {
 	db := c.client.Database(dbName)
 
 	result := db.RunCommand(ctx, bson.D{
@@ -310,10 +325,7 @@ func (c *Client) DeleteDBUser(ctx context.Context, dbName, userName string) erro
 	if err := result.Decode(&response); err != nil {
 		return err
 	}
-	if err := validateResponse(response); err != nil {
-		return err
-	}
-	return nil
+	return validateResponse(response)
 }
 
 func validateResponse(response CommandResponse) error {
